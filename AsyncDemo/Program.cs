020389d3ba3dfa93524089f2819c6ee7febb26ee
Tung Nguyen <ngoctung@floatingcube.com>
2017-10-10 09:37:44 +0700

    add more example

---
+++ AsyncDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /* Example 1: Basic async operation
+            Task t = AsynchronyWithAwait();
+            t.Wait(); */
+
+            /* Example 2: Using async with lambda
+            Task t = AsyncProcessing();
+            t.Wait(); */
+
+            /*ex 3 */
+            Task t = AsynchronyWithAwait();
+            t.Wait();
+        }
+
+        private static async Task AsynchronyWithAwait()
+        {
+            try
+            {
+                string result = await GetInfoAsync("Async Task 1");
+                Console.WriteLine(result);
+                //result = await GetInfoAsync("Async Task 2");
+                //Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static async Task AsyncProcessing()
+        {
+            Func<string, Task<string>> asyncLambda = async name =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                return $"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}." +
+                       $"Is thread pool thread: {Thread.CurrentThread.IsThreadPoolThread}";
+            };
+            string result = await asyncLambda("async Lambda");
+            Console.WriteLine(result);
+        }
+
+        private static async Task<string> GetInfoAsync(string name)
+        {
+            await Task.Delay((TimeSpan.FromSeconds(2)));
+            return $"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}." +
+                   $"Is thread pool thread: {Thread.CurrentThread.IsThreadPoolThread}";
+        }
+    }
+}
--- ThreadDemo/Program.cs
+++ ThreadDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -66,6 +67,33 @@ namespace ThreadDemo
 
             threadOne.Start();
             threadTwo.Start(); */
+
+            /*Example 7: Thread Safe
+            Console.WriteLine("Incorrect counter:");
+            var c = new Counter();
+            var t1 = new Thread(() => TestCounter(c));
+            var t2 = new Thread(() => TestCounter(c));
+            var t3 = new Thread(() => TestCounter(c));
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            Console.WriteLine($"Total Count: {c.Count}");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Correct Counter");
+            var c1 = new CounterWithLock();
+            t1 = new Thread(() => TestCounter(c1));
+            t2 = new Thread(() => TestCounter(c1));
+            t3 = new Thread(() => TestCounter(c1));
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            Console.WriteLine($"Total count: {c1.Count}"); */
         }
 
         // Example 1 Method
@@ -116,6 +144,16 @@ namespace ThreadDemo
             Thread.Sleep(TimeSpan.FromSeconds(2));
             sample.Stop();
         }
+
+        // Example 7
+        private static void TestCounter(CounterBase c)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                c.Increment();
+                c.Decrement();
+            }
+        }
     }
 
     // Example 5 class
@@ -151,5 +189,49 @@ namespace ThreadDemo
                 Console.WriteLine($"{Thread.CurrentThread.Name} prints { i }");
             }
         }
+    }
+
+    internal class CounterWithLock : CounterBase
+    {
+        private readonly object _syncRoot = new Object();
+        public int Count { get; set; }
+
+        public override void Increment()
+        {
+            lock (_syncRoot)
+            {
+                Count++;
+            }
+        }
+
+        public override void Decrement()
+        {
+            lock (_syncRoot)
+            {
+                Count--;
+            }
+        }
+    }
+
+    internal class Counter : CounterBase
+    {
+        public int Count { get; private set; }
+
+        public override void Increment()
+        {
+            Count++;
+        }
+
+        public override void Decrement()
+        {
+            Count--;
+        }
+    }
+
+    internal abstract class CounterBase
+    {
+        public abstract void Increment();
+
+        public abstract void Decrement();
     }
 }
